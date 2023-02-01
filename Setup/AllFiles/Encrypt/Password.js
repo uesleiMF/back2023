6edcdb86aef2a9107ea7312ae23b9cacaa8c04d6
Ueslei <joseueslei2@gmail.com>
2023-02-01 17:45:48 -0300

    123

--- Setup/AllFiles/Encrypt/Password.js
+++
@@ -1,8 +0,0 @@
-const crypto=require("crypto-js");
-module.exports.Encrypt= (password)=>{
-    return crypto.AES.encrypt(password,"secretkey").toString();
-}
-module.exports.Decrypt=(encodedPass,password)=>{
-    const hash=crypto.AES.decrypt(encodedPass,"secretkey").toString(crypto.enc.Utf8);
-    return hash===password;
-}
