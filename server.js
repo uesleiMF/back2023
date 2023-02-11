29c2dda9d3be0ba8caca57ef03c0417aa1c88717
Ueslei <joseueslei2@gmail.com>
2023-02-11 17:15:21 -0300

    123

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://front2023u.vercel.app"],
+    origin: ["http://localhost:3000", "https://redeamaiieq.vercel.app"],
     credentials: true,
   })
 );
