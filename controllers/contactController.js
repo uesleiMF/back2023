2cc4f8eab5b93b013721b64995794853f71a53cb
Ueslei <joseueslei2@gmail.com>
2023-02-11 19:26:38 -0300

    123

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -22,7 +22,7 @@ const contactUs = asyncHandler(async (req, res) => {
   const reply_to = user.email;
   try {
     await sendEmail(subject, message, send_to, sent_from, reply_to);
-    res.status(200).json({ success: true, message: "Email Sent" });
+    res.status(200).json({ success: true, message: "E-mail enviado" });
   } catch (error) {
     res.status(500);
     throw new Error("E-mail não enviado, tente novamente");
